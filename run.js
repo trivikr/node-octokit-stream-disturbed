c6f3193de312f99315d82a61fe069cb06a16bc55
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-06-23 10:36:16 -0700

    chore: change code to upload zip from createReadStream

--- run.js
+++ run.js
@@ -1,10 +1,11 @@
+import { createReadStream, statSync } from "fs";
 import { Octokit } from "@octokit/rest";
 
 const auth = process.env.OCTOKIT_AUTH_TOKEN;
 const owner = "trivikr"; // Replace with your username
 const repo = "node-octokit-stream-disturbed"; // Replace with your fork name, if different.
 const tag = "v0.0.1"; // Replace with the tag you want to edit.
-const name = "test";
+const name = "hello_world.zip";
 
 const octokit = new Octokit({ auth });
 
@@ -16,22 +17,18 @@ if (asset_id) {
 }
 
 const release_id = release.data.id;
-const chunks = ["hello", "world"];
-const data = new ReadableStream({
-  start(controller) {
-    chunks.forEach((chunk) => controller.enqueue(chunk));
-    controller.close();
-  },
-});
+
+const data = createReadStream(name);
+const dataLength = statSync(name).size;
 
 await octokit.rest.repos.uploadReleaseAsset({
   owner,
   repo,
   release_id,
-  name: "test",
+  name,
   headers: {
-    "content-length": chunks.join("").length,
-    "content-type": "text",
+    "content-length": dataLength,
+    "content-type": "application/zip",
   },
   data,
 });
